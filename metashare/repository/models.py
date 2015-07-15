8ad1185641b5424db624807f49e85240f9cd6ea3
Miltos Deligiannis <mdeligiannis@outlook.com>
2015-07-15 12:11:32 +0300

    modify audioFormatInfoType_model mimetype verbose_name

--- metashare/repository/models.py
+++ metashare/repository/models.py
@@ -4277,11 +4277,15 @@ class audioFormatInfoType_model(SchemaModel):
 
     mimeType = XmlCharField(
       verbose_name='Mime type', 
-      help_text='The mime-type of the resource which is a formalized spe' \
-      'cifier for the format included or a mime-type that the tool/servi' \
-      'ce accepts; value to be taken from a subset of the official mime ' \
-      'types of the Internet Assigned Numbers Authority (http://www.iana' \
-      '.org/)',
+      help_text='The mime-type of the resource which is a formalized' \
+            'specifier for the format included or a mime-type ' \
+            'that the tool/service accepts, in conformance' \
+            'with the values of the IANA (Internet Assigned ' \
+            'Numbers Authority); you can select one of ' \
+            'the pre-defined values or add a value, ' \
+            'PREFERABLY FROM THE IANA MEDIA MIMETYPE ' \
+            'RECOMMENDED VALUES (http://www.iana.org/assignments/' \
+            'media-types/media-types.xhtml)',
       max_length=50, )
 
     signalEncoding = MultiSelectField(
