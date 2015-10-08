d4f9118035cfc93d53bd0b44e7afc2b59741ac27
Miltos Deligiannis <mdeligiannis@outlook.com>
2015-10-08 22:02:31 +0300

    modify prettify camel case to handle strings such as ODbl (not O Dbl)

--- metashare/utils.py
+++ metashare/utils.py
@@ -57,12 +57,13 @@ def prettify_camel_case_string(cc_str):
     '''
     result = cc_str
     if len(result) > 1:
-        # result = result.replace('-', ' - ')
-        result = re.sub(r'(.)(?=[A-Z][a-z])', r'\1 ', result)
+        # result = result.replace('-', ' - ')  AtA
+        result = result.replace('_', ' ')
+        result = result.replace('AtA', 'At a')
+        result = re.sub(r'(..)(?=[A-Z][a-z])', r'\1 ', result)
         result = ' '.join([(len(token) > 1 and (token[0].upper() + token[1:]))
                            or token[0].upper() for token in result.split()])
     return result
-
 
 def create_breadcrumb_template_params(model, action):
     '''
