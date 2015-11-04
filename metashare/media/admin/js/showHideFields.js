6ff7cf2d718978a6b7dd11102497dd15a7fa6ba2
Miltos Deligiannis <mdeligiannis@outlook.com>
2015-11-04 10:32:31 +0200

    .

--- metashare/media/admin/js/showHideFields.js
+++ metashare/media/admin/js/showHideFields.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
     // show conditionsOfUse field only for specific licences
-    var licences = ["openForReuseWithRestrictions", "non-standard_Other_Licence_Terms", "underNegotiation"];
-    $(window).load(function () {
+    var licences = ["openForReuseWithRestrictions", "non-standard/Other_Licence/Terms", "underNegotiation"];
+
         $("select option:selected").each(function () {
-            if ($(this).val() != "non-standard_Other_Licence_Terms") {
+            if ($(this).val() != "non-standard/Other_Licence/Terms") {
                 $(this).parent().parent().parent().siblings(".form-row.otherLicenceName").hide();
+                $(this).parent().parent().parent().siblings(".form-row.termsOfUseText").hide();
+                $(this).parent().parent().parent().siblings(".form-row.termsOfUseURL").hide();
             }
             if (jQuery.inArray($(this).val(), licences) != -1) {
                 $(this).parent().parent().parent().siblings(".form-row.restrictionsOfUse").show();
@@ -12,26 +14,30 @@ $(document).ready(function () {
             else {
                 $(this).parent().parent().parent().siblings(".form-row.restrictionsOfUse").hide();
             }
-        });
+
     })
 
 
-    $('select').on('change', function () {
+    $('select').change(function () {
         if (jQuery.inArray($(this).val(), licences) != -1) {
             $(this).parent().parent().siblings(".form-row.restrictionsOfUse").show();
         }
         else {
             $(this).parent().parent().siblings(".form-row.restrictionsOfUse").hide();
         }
-        if ($(this).val() == 'non-standard_Other_Licence_Terms') {
+        if ($(this).val() == 'non-standard/Other_Licence/Terms') {
             $(this).parent().parent().siblings(".form-row.otherLicenceName").show();
             $(this).parent().parent().siblings(".form-row.otherLicenceName").find("input").removeAttr('disabled');
+            $(this).parent().parent().siblings(".form-row.termsOfUseText").show();
+            $(this).parent().parent().siblings(".form-row.termsOfUseURL").show();
         }
         else {
             $(this).parent().parent().siblings(".form-row.otherLicenceName").hide();
             $(this).parent().parent().siblings(".form-row.otherLicenceName").find("input").attr('disabled', 'disabled');
+            $(this).parent().parent().siblings(".form-row.termsOfUseText").hide();
+            $(this).parent().parent().siblings(".form-row.termsOfUseURL").hide();
         }
-    })
+    });
 
     //show additional information if respective boolean field is checked
     $("input[id*='personalDataIncluded']").each(function () {
