53652fdf23331126da0bf13367b1a4cfb1dfe7bf
Miltos Deligiannis <mdeligiannis@outlook.com>
2016-01-27 10:04:35 +0200

    enhance licencing display in search results

--- metashare/repository/templatetags/resource_distribution.py
+++ metashare/repository/templatetags/resource_distribution.py
@@ -9,9 +9,13 @@ register = template.Library()
 def licence_icon(licence):
     if licence == "non-standard/Other_Licence/Terms":
         img_location = "{}images/licence_icons/licences/other.png".format(MEDIA_URL)
+    elif licence.startswith("CC"):
+        img_location = "{}images/licence_icons/licences/cc.png".format(MEDIA_URL)
+    elif licence == ("underNegotiation"):
+        img_location = "{}images/licence_icons/licences/negotiate.png".format(MEDIA_URL)
     else:
-        img_location = "{}images/licence_icons/licences/{}.png".format(MEDIA_URL,licence)
-    return u"".join(u"<img src=\"{}\" title=\"{}\" alt=\"{}\" width=\"80\" height=\"15\"/>" \
+        img_location = "{}images/licence_icons/licences/open_licence.png".format(MEDIA_URL)
+    return u"".join(u"<img style=\"padding:1px\" src=\"{}\" title=\"{}\" alt=\"{}\" width=\"17\" height=\"17\"/>" \
                     .format(img_location, pretty_camel(licence), pretty_camel(licence)))
 
 
@@ -20,7 +24,7 @@ register.tag('licence_icon', licence_icon)
 @register.filter("condition_icon")
 def condition_icon(condition):
     img_location = "{}images/licence_icons/conditions/{}.png".format(MEDIA_URL,condition)
-    return u"".join(u"<img src=\"{}\" title=\"{}\" alt=\"{}\" width=\"15\" height=\"15\"/>" \
+    return u"".join(u"<img style=\"padding:1px\" src=\"{}\" title=\"{}\" alt=\"{}\" width=\"17\" height=\"17\"/>" \
                     .format(img_location, pretty_camel(condition), pretty_camel(condition)))
 
 
