50d2d4cfd918a7675c302b755405704fc7705994
Miltos Deligiannis <mdeligiannis@outlook.com>
2016-03-17 15:00:43 +0200

    view logged in users

--- metashare/repository/views.py
+++ metashare/repository/views.py
@@ -17,6 +17,8 @@ from lxml import etree
 import shutil
 
 
+from django.contrib.sessions.models import Session
+
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 from django.contrib import messages
@@ -58,6 +60,19 @@ MAXIMUM_READ_BLOCK_SIZE = 4096
 LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(LOG_HANDLER)
 
+def get_all_logged_in_users():
+    # Query all non-expired sessions
+    # use timezone.now() instead of datetime.now() in latest versions of Django
+    sessions = Session.objects.filter(expire_date__gte=datetime.datetime.now())
+    uid_list = []
+
+    # Build a list of user ids from that query
+    for session in sessions:
+        data = session.get_decoded()
+        uid_list.append(data.get('_auth_user_id', None))
+
+    # Query all logged in users based on id list
+    return User.objects.filter(id__in=uid_list)
 
 def _convert_to_template_tuples(element_tree):
     """
