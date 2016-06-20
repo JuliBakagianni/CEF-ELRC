7091eff561345faff43d6091307b9b67a6b04104
MiltosD <mdeligiannis@outlook.com>
2016-06-20 11:49:23 +0300

    fix mimetype label template tag

--- metashare/repository/templatetags/mimetype_label.py
+++ metashare/repository/templatetags/mimetype_label.py
@@ -10,7 +10,10 @@ def mimetype_label(input):
     output = []
     for m in mimetypes:
         # um = unicode(m, "utf-8")
-        output.append(MIMETYPEVALUE_TO_MIMETYPELABEL[m.replace(u' ', u'').lower()])
+        if m.replace(u' ', u'').lower() in MIMETYPEVALUE_TO_MIMETYPELABEL:
+            output.append(MIMETYPEVALUE_TO_MIMETYPELABEL[m.replace(u' ', u'').lower()])
+        else:
+            output.append(m)
     return ', '.join(output)
 
 register.tag('mime_label', mimetype_label)
